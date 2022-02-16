9d7277179710ac11886c0e5f084575b56eda77b2
Никита Хромов <chromovnick@gmail.com>
2022-02-16 23:26:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17
+{
+    class Account <T>
+    {
+        public T Number { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+
+        public string GetInfo()
+        {
+            return $"{Number} {Balance} {Name}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите ФИО владельца счета");
+            Name = Console.ReadLine();
+        }
+    }
+}
